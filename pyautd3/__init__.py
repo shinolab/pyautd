70f84db12b3c014ac5fdb6433d8e8856943a3f66
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2020-11-16 15:37:43 +0900

    v0.7.1

--- pyautd3/__init__.py
+++ pyautd3/__init__.py
@@ -4,7 +4,7 @@ Project: pyautd
 Created Date: 11/02/2020
 Author: Shun Suzuki
 -----
-Last Modified: 06/11/2020
+Last Modified: 16/11/2020
 Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 -----
 Copyright (c) 2020 Hapis Lab. All rights reserved.
@@ -45,7 +45,7 @@ __all__ = [
     'Sequence',
     'AUTD',
     'Link']
-__version__ = '0.7.0'
+__version__ = '0.7.1'
 
 LIB_PATH = os.path.join(os.path.dirname(__file__), 'bin', PREFIX + 'autd3capi' + EXT)
 Nativemethods().init_dll(LIB_PATH)
